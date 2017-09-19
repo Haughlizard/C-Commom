f35b6f1206eb325f704a6288169a0e54dcae329c
Haughlizard <lishengyan1991@qq.com>
2017-09-19 14:41:42 +0800

    逆序输出数组
    逆序输出数组

---
+++ Reverse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms
+{
+    class Reverse
+    {
+        public static void ReverseList(int[] list)
+        {
+            if (list.Length > 0)
+            {
+                int mid = list.Length / 2;
+                for (int i = 0; i < mid; i++)
+                {
+                    int temp = list[i];
+                    list[i] = list[list.Length - 1 - i];
+                    list[list.Length - 1 - i] = temp;
+                }
+            }
+        }
+    }
+}
